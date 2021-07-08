df169d2bb6db72cbed53d58f8bed1f2fc7423e82
yskim <acabia@naver.com>
2021-07-08 18:20:24 +0900

    django lession 18 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,8 +1,10 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 
 # Create your views here.
+from django.urls import reverse
+
 from accountapp.models import HelloWorld
 
 
@@ -13,7 +15,13 @@ def hello_world(request):
         hello_world = HelloWorld()
         hello_world.text = str
         hello_world.save()      #save to DB
+
+        hello_world_list = HelloWorld.objects.all()
         # return render(request, 'accountapp/hello_world.html', context={'text': str})
-        return render(request, 'accountapp/hello_world.html', context={'hello_world_output': hello_world})
+        # return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
+        # Post 후 Get 방식으로 변경한다.
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
     else:
-        return render(request, 'accountapp/hello_world.html', context={'text': str})
+        hello_world_list = HelloWorld.objects.all()
+        # return render(request, 'accountapp/hello_world.html', context={'text': str})
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
