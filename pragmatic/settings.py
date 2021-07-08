4106de9521747930a53588a6d1fad1b75ba13002
yskim <acabia@naver.com>
2021-07-08 12:35:37 +0900

    course 10-1 commit

--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -135,7 +135,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # 프로젝트 종속된 static 변수 저장 폴더
+STATICFILES_DIRS = [  # 프로젝트 종속하지 않은 static 변수 저장 폴더
+    BASE_DIR / 'static',
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
